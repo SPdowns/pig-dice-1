6402c0480757cb3eb826e9663920583a214ef4c1
Kate Skorija <kateskorija@gmail.com>
2020-06-17 13:42:40 -0500

    add User Interface Logic to display rollScore and roundScore to DOM
    Co-authored-by: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,23 @@ function Player(rollScore, roundScore, totalScore) {
 
 Player.prototype.roll = function() {
   playerRoll = (Math.floor(Math.random() *6) +1);
-  this.rollScore += playerRoll
+  this.rollScore = playerRoll
+  this.roundScore += playerRoll
 }
 
-let playerOne = new Player(0, 0, 0);
-playerOne.roll();
-console.log(playerOne);
+
+
+
+// User Interface Logic ----------------
+
+$(document).ready(function() {
+  let playerOne = new Player(0, 0, 0);
+  $("button#p1-roll").click(function() {
+    playerOne.roll();
+
+    $("#roll-score").text(playerOne.rollScore);
+    $("#p1-round-score").text(playerOne.roundScore);
+    console.log(playerOne.rollScore);
+  })
+})
+
