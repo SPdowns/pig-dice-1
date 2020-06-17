fb941a06e1bae46d2483699f42f062cd3522e10b
Kate Skorija <kateskorija@gmail.com>
2020-06-17 15:26:07 -0500

    add UI functionality for player two buttons and totals
    Co-authored-by: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,10 @@ Player.prototype.pass = function() {
   this.totalScore += this.roundScore;
 }
 
+Player.prototype.turnEnd = function() {
+
+}
+
 
 // User Interface Logic ----------------
 
@@ -27,16 +31,35 @@ $(document).ready(function() {
   $("button#p1-roll").click(function() {
     playerOne.roll();
 
+
+    document.getElementById('sound1').play();
     $("#roll-score").text(playerOne.rollScore);
     $("#p1-round-score").text(playerOne.roundScore);
-  })
+  });
 
   $("button#p1-pass").click(function() {
     playerOne.pass();
-    playerOne.roundScore = "0";
+    playerOne.roundScore = 0;
 
     $("#p1-total-score").text(playerOne.totalScore);
     $("#p1-round-score").text(playerOne.roundScore);
   });
-})
+
+  $("button#p2-roll").click(function() {
+    playerOne.roll();
+
+
+    document.getElementById('sound1').play();
+    $("#roll-score").text(playerOne.rollScore);
+    $("#p2-round-score").text(playerOne.roundScore);
+  });
+
+  $("button#p2-pass").click(function() {
+    playerOne.pass();
+    playerOne.roundScore = 0;
+
+    $("#p2-total-score").text(playerOne.totalScore);
+    $("#p2-round-score").text(playerOne.roundScore);
+  });
+});
 
